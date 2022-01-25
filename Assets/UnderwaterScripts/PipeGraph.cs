3841f218fcd10c90a291f4631e2144fa43580db7
kuckertza <kuckertza@gmail.com>
2022-01-24 18:20:35 -0600

    Updated checkValid()

--- Assets/UnderwaterScripts/PipeGraph.cs
+++ Assets/UnderwaterScripts/PipeGraph.cs
@@ -47,19 +47,19 @@ namespace Graphs
             PipeNode node1 = Find(id1);
             PipeNode node2 = Find(id2);
 
-            if(node1 == null || node2 == null)
+            if (node1 == null || node2 == null)
             {
                 return false;
             }
-            else if(node1.Neighbors.Contains(node2))
+            else if (node1.Neighbors.Contains(node2))
             {
                 // already exists
                 return false;
             }
             else
             {
-                return node1.AddNeighbor(node2, dir) && 
-                    node2.AddNeighbor(node1, (dir+2)%4);
+                return node1.AddNeighbor(node2, dir) &&
+                    node2.AddNeighbor(node1, (dir + 2) % 4);
             }
         }
 
@@ -83,7 +83,7 @@ namespace Graphs
         {
             PipeNode removeNode = Find(id);
 
-            if(removeNode == null)
+            if (removeNode == null)
             {
                 return false;
             }
@@ -121,60 +121,24 @@ namespace Graphs
                 return true;
             }
         }
-
-        /*
-        public bool AddNode(PipeNode newPipe)
-        {
-            for (int i = 0; i < 4; ++i)
-            {
-                if (cur.Shape[i] == 1 && newPipe.Shape[(i + 2) % 4] == 1)
-                {
-                    // TODO: how to determine which openning to connect?
-                    // How to go back to previous pipe if need to?
-                    // need memory of all pipe in the graph? use matrix or list to connect?
-                    // is 4 direction pipes still needed?
-                    newPipe.AddNeighbor(newPipe, (i + 2) % 4);
-                    cur.Shape[i] = 0;
-                    newPipe.Shape[(i + 2) % 4] = 0;
-                    cur = newPipe;
-                    return true;
-                }
-            }
-
-            // invalid placement
-            return false;
-        }
-
-        public bool removeLastPlaced()
-        {
-            PipeNode tmp = cur;
-            // which one to go back to as cur?
-
-            for (int i = 0; i < 4; ++i)
-            {
-                if (tmp.Shape[i] == 0 && tmp.Neighbours[i] != null)
-                {
-                    cur = tmp.Neighbours[i];
-                    cur.Shape[(i + 2) % 4] = 0;
-                    cur.Neighbours[(i + 2) % 4] = null;
-                    // how to get the node off the grid?
-                    return true;
-                }
-            }
-
-            // nothing to remove here
-            return false;
-        }
-
-        public bool removeNode(PipeNode rm)
-        {
-
-        }
-
         public bool checkValid()
         {
-
+            //if(connected to last node){
+            //TODO: How to know you've connected to the last node?
+            //
+            foreach (PipeNode node in nodes)
+            {
+                //TODO: How to tell the difference between a cell in neighbors being null because 
+                //it hasn't been  connected to anything and is therefore a leak, and it being null
+                //because it's a shape with less than four openings. Create arrays of differing sizes based off shape?
+                foreach (PipeNode neighbor in node.Neighbors)
+                    if (neighbor == null)
+                    {
+                        return false;
+                    }
+            }
+            return true;
+            //}
         }
-        */
     }
 }
