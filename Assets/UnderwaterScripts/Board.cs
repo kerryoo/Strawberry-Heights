8c562e58dda97ffd23603bb55c4ab44b47f2f6c3
Chang Guo <chang.guo@vanderbilt.edu>
2022-01-26 20:03:35 -0600

    skeleton board

---
+++ Assets/UnderwaterScripts/Board.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board
+{
+    // TODO: start and end position vectors
+    int level;
+
+    //TODO: constructor
+
+    public void ResetLevel()
+    {
+
+    }
+
+    public void SpawnObstacles()
+    {
+
+    }
+
+    // a further abstraction that calls reset board and spawn obstacles
+    public void OnComplete()
+    {
+
+    }
+
+    // checks if there’s a valid path and if so, makes a water flow
+    // animation and calls onComplete
+    public void Sumit()
+    {
+
+    }
+
+    // if submission fails, show the water flow fail
+    public void OnFail()
+    {
+
+    }
+}
