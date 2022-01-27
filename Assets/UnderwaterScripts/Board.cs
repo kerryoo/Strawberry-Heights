c7f73bc19e4e95d91e3360bd2365154c567b5da9
Chang Guo <chang.guo@vanderbilt.edu>
2022-01-26 20:32:53 -0600

    first draft board

--- Assets/UnderwaterScripts/Board.cs
+++ Assets/UnderwaterScripts/Board.cs
@@ -2,28 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// DRAFT
 public class Board
 {
     // TODO: start and end position vectors
-    PipeGraph graph;
+    PipeGraph graph; // operate on graph
+    PipeGraph origGraph;
     int level;
 
     //TODO: constructor
 
     public void ResetLevel()
     {
-
+        graph = origGraph;
     }
 
     // randomly spawn obstacles on the board
     public void SpawnObstacles()
     {
-
+        // put obstacles on origGraph
+        graph = origGraph;
     }
 
     // a further abstraction that calls reset board and spawn obstacles
     public void OnComplete()
     {
+        level += 1;
+        origGraph = new PipeGraph;
+        SpawnObstacles();
 
     }
 
@@ -31,7 +37,14 @@ public class Board
     // animation and calls onComplete
     public void Submit()
     {
-
+        if (graph.checkValid())
+        {
+            OnComplete();
+        }
+        else
+        {
+            OnFail();
+        }
     }
 
     // if submission fails, show the water flow fail
