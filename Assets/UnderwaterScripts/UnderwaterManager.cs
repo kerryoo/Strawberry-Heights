5bc7cf9a91a73e98c1c50d64ec79b95abc9d08b8
kuckertza <kuckertza@gmail.com>
2022-02-02 16:08:53 -0600

    Worked on UnderwaterManager

--- Assets/UnderwaterScripts/UnderwaterManager.cs
+++ Assets/UnderwaterScripts/UnderwaterManager.cs
@@ -4,5 +4,20 @@ using UnityEngine;
 
 public class UnderwaterManager : GameManager
 {
+    public GameObject pipe;
+    int s;
+    
+    //sets the rate at which pipes spawn
+    void setSpawningRate(int seconds)
+    {
+        s = seconds;
+    }
 
+    //will have to consider how to make sure pipes don't spawn where there are already objects
+    void update()
+    {
+        if (Time.time % s == 0) {
+            Instantiate(pipe); //TODO:Add random position parameter
+        }
+    }
 }
