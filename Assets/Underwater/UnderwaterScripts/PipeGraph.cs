5fb00120fe5cfb0507c62897da938cd082bc7bda
kerryoo <kerr.yen.yoo@vanderbilt.edu>
2022-02-06 15:53:57 -0600

    Edits to PipeNode

--- Assets/Underwater/UnderwaterScripts/PipeGraph.cs
+++ Assets/Underwater/UnderwaterScripts/PipeGraph.cs
@@ -6,149 +6,146 @@ using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
 
-namespace Graphs
+public class PipeGraph : MonoBehaviour
 {
+    List<PipeNode> nodes = new List<PipeNode>();
 
-    public class PipeGraph
+    // how to note positions?
+    PipeNode startPos;
+    PipeNode endPos;
+
+    public PipeGraph(PipeNode start, PipeNode end)
     {
-        List<PipeNode> nodes = new List<PipeNode>();
-        // how to note positions?
-        PipeNode startPos;
-        PipeNode endPos;
+        this.startPos = start;
+        this.endPos = end;
+    }
 
-        public PipeGraph(PipeNode start, PipeNode end)
+    public IList<PipeNode> Nodes
+    {
+        get { return nodes.AsReadOnly(); }
+    }
+
+    public bool Connect(int id1, int id2, int dir)
+    {
+        PipeNode node1 = Find(id1);
+        PipeNode node2 = Find(id2);
+
+        if (node1 == null || node2 == null)
         {
-            this.startPos = start;
-            this.endPos = end;
-        }
-
-        public IList<PipeNode> Nodes
-        {
-            get { return nodes.AsReadOnly(); }
-        }
-
-        public bool Connect(int id1, int id2, int dir)
-        {
-            PipeNode node1 = Find(id1);
-            PipeNode node2 = Find(id2);
-
-            if (node1 == null || node2 == null)
-            {
-                return false;
-            }
-            else if (node1.Neighbors.Contains(node2))
-            {
-                // already exists
-                return false;
-            }
-            else
-            {
-                return node1.AddNeighbor(node2, dir) &&
-                    node2.AddNeighbor(node1, (dir + 2) % 4);
-            }
-        }
-
-        /// <summary>
-        /// Finds the graph node with the given value
-        /// </summary>
-        /// <returns>graph node or null if not found</returns>
-        public PipeNode Find(int id)
-        {
-            foreach (PipeNode node in nodes)
-            {
-                if (node.Id.Equals(id))
-                {
-                    return node;
-                }
-            }
-            return null;
-        }
-
-        public bool RemoveNode(int id)
-        {
-            PipeNode removeNode = Find(id);
-
-            if (removeNode == null)
-            {
-                return false;
-            }
-            else
-            {
-                nodes.Remove(removeNode);
-
-                foreach (PipeNode node in nodes)
-                {
-                    node.RemoveNeighbor(removeNode);
-                }
-
-                return true;
-            }
-        }
-
-        public bool RemoveEdge(int id1, int id2)
-        {
-            PipeNode node1 = Find(id1);
-            PipeNode node2 = Find(id2);
-
-            if (node1 == null || node2 == null)
-            {
-                return false;
-            }
-            else if (!node1.Neighbors.Contains(node2))
-            {
-                // does not exist
-                return false;
-            }
-            else
-            {
-                node1.RemoveNeighbor(node2);
-                node2.RemoveNeighbor(node1);
-                return true;
-            }
-        }
-        public bool checkValid()
-        { 
-            foreach (PipeNode node in nodes)
-            {
-                int count = 0;
-                foreach (char c in node.Shape)
-                {
-                    if(c == '1')
-                    {
-                        ++count;
-                    }
-                }
-                
-                if(count != node.Neighbors.Length)
-                {
-                    return false;
-                }
-            }
-
-            return dfs(startPos);
-        }
-
-        HashSet<PipeNode> visited;
-        public bool dfs(PipeNode node)
-        {
-            if(node.Id == 1)
-            {
-                return true;
-            }
-
-            if(!visited.Contains(node))
-            {
-                visited.Add(node);
-                if(dfs(node.Neighbors[0]) || dfs(node.Neighbors[1]) || dfs(node.Neighbors[2]) || dfs(node.Neighbors[3]))
-                {
-                    return true;
-                }
-
-                visited.Remove(node);
-            }
-
             return false;
         }
-
+        else if (node1.Neighbors.Contains(node2))
+        {
+            // already exists
+            return false;
+        }
+        else
+        {
+            return node1.AddNeighbor(node2, dir) &&
+                node2.AddNeighbor(node1, (dir + 2) % 4);
+        }
     }
+
+    /// <summary>
+    /// Finds the graph node with the given value
+    /// </summary>
+    /// <returns>graph node or null if not found</returns>
+    public PipeNode Find(int id)
+    {
+        foreach (PipeNode node in nodes)
+        {
+            if (node.Id.Equals(id))
+            {
+                return node;
+            }
+        }
+        return null;
+    }
+
+    public bool RemoveNode(int id)
+    {
+        PipeNode removeNode = Find(id);
+
+        if (removeNode == null)
+        {
+            return false;
+        }
+        else
+        {
+            nodes.Remove(removeNode);
+
+            foreach (PipeNode node in nodes)
+            {
+                node.RemoveNeighbor(removeNode);
+            }
+
+            return true;
+        }
+    }
+
+    public bool RemoveEdge(int id1, int id2)
+    {
+        PipeNode node1 = Find(id1);
+        PipeNode node2 = Find(id2);
+
+        if (node1 == null || node2 == null)
+        {
+            return false;
+        }
+        else if (!node1.Neighbors.Contains(node2))
+        {
+            // does not exist
+            return false;
+        }
+        else
+        {
+            node1.RemoveNeighbor(node2);
+            node2.RemoveNeighbor(node1);
+            return true;
+        }
+    }
+    public bool checkValid()
+    { 
+        foreach (PipeNode node in nodes)
+        {
+            int count = 0;
+            foreach (char c in node.Shape)
+            {
+                if(c == '1')
+                {
+                    ++count;
+                }
+            }
+                
+            if(count != node.Neighbors.Length)
+            {
+                return false;
+            }
+        }
+
+        return dfs(startPos);
+    }
+
+    HashSet<PipeNode> visited;
+    public bool dfs(PipeNode node)
+    {
+        if(node.Id == 1)
+        {
+            return true;
+        }
+
+        if(!visited.Contains(node))
+        {
+            visited.Add(node);
+            if(dfs(node.Neighbors[0]) || dfs(node.Neighbors[1]) || dfs(node.Neighbors[2]) || dfs(node.Neighbors[3]))
+            {
+                return true;
+            }
+
+            visited.Remove(node);
+        }
+
+        return false;
+    }
+
 }
--- Assets/Underwater/UnderwaterScripts/PipeNode.cs
+++ Assets/Underwater/UnderwaterScripts/PipeNode.cs
@@ -2,90 +2,105 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Graphs
+
+public class PipeNode : MonoBehaviour
 {
-    public class PipeNode
+    [SerializeField] List<GameObject> connectPoints;
+
+    [SerializeField] int id;
+
+    bool beingHeld;
+    string shape; // 4-digit binary indicating the shape of the pipe (int values 0000 to 1111)
+    PipeNode[] neighbors; // array of node representing North, East, South, West nieghbours
+
+    private void Update()
     {
-        int id;
-        string shape; // 4-digit binary indicating the shape of the pipe (int values 0000 to 1111)
-        PipeNode[] neighbors; // array of node representing North, East, South, West nieghbours
+        GetConnectPointPos();
+    }
 
-        // CONSTRUCTOR
-        public PipeNode(int id, string shape)
+    // CONSTRUCTOR
+    public PipeNode(int id, string shape)
+    {
+        this.id = id;
+        this.shape = shape;
+        neighbors = new PipeNode[4];
+    }
+
+    // PROPERTIES
+    // Get ID
+    public int Id
+    {
+        get { return id; }
+    }
+
+    // Get shape of node
+    public string Shape
+    {
+        get { return shape; }
+    }
+
+    // Get read-only list of neighbours of the node
+    public PipeNode[] Neighbors
+    {
+        get { return neighbors; }
+    }
+
+    //METHODS
+    // add new neightbour to specified direction (N = 0, E = 1, S = 2, W = 3)
+    public bool AddNeighbor(PipeNode neighbor, int direction)
+    {
+        // check valid here
+        if (shape[direction] == 1 && neighbor.shape[(direction + 2) % 4] == 1
+            && neighbors[direction] == null && neighbor.neighbors[(direction + 2) % 4] == null)
         {
-            this.id = id;
-            this.shape = shape;
-            neighbors = new PipeNode[4];
+            neighbors[direction] = neighbor;
+            return true;
         }
 
-        // PROPERTIES
-        // Get ID
-        public int Id
-        {
-            get { return id; }
-        }
+        return false;
+    }
 
-        // Get shape of node
-        public string Shape
+    // remove a node
+    public bool RemoveNeighbor(PipeNode neighbor)
+    {
+        for(int i =0; i<neighbors.Length; ++i)
         {
-            get { return shape; }
-        }
-
-        // Get read-only list of neighbours of the node
-        public PipeNode[] Neighbors
-        {
-            get { return neighbors; }
-        }
-
-        //METHODS
-        // add new neightbour to specified direction (N = 0, E = 1, S = 2, W = 3)
-        public bool AddNeighbor(PipeNode neighbor, int direction)
-        {
-            // check valid here
-            if (shape[direction] == 1 && neighbor.shape[(direction + 2) % 4] == 1
-                && neighbors[direction] == null && neighbor.neighbors[(direction + 2) % 4] == null)
-            {
-                neighbors[direction] = neighbor;
-                return true;
-            }
-
-            return false;
-        }
-
-        // remove a node
-        public bool RemoveNeighbor(PipeNode neighbor)
-        {
-            for(int i =0; i<neighbors.Length; ++i)
-            {
-                if(neighbors[i] == neighbor)
-                {
-                    neighbors[i] = null;
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        // remove all neighbours
-        public void RemoveAllNeighbors()
-        {
-            for (int i = 0; i < neighbors.Length; i++)
+            if(neighbors[i] == neighbor)
             {
                 neighbors[i] = null;
+                return true;
             }
         }
+        return false;
+    }
 
-        //rotate pipe by modifying shapes
-        public void RotateRight()
+    // remove all neighbours
+    public void RemoveAllNeighbors()
+    {
+        for (int i = 0; i < neighbors.Length; i++)
         {
-            RemoveAllNeighbors();
-            shape = shape[3] + shape.Substring(0, 3);
+            neighbors[i] = null;
         }
+    }
 
-        public void RotateLeft()
+    //rotate pipe by modifying shapes
+    public void RotateRight()
+    {
+        RemoveAllNeighbors();
+        shape = shape[3] + shape.Substring(0, 3);
+    }
+
+    public void RotateLeft()
+    {
+        RemoveAllNeighbors();
+        shape = shape.Substring(1, 4) + shape[0];
+    }
+
+    private void GetConnectPointPos()
+    {
+        foreach (GameObject connectPoint in connectPoints)
         {
-            RemoveAllNeighbors();
-            shape = shape.Substring(1, 4) + shape[0];
+            Debug.Log(connectPoint.transform.position);
         }
     }
 }
